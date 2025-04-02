b3f84bc556e7d4bf7a44708c145f2b86f227d3b2
pigeonligh <pigeonligh@hotmail.com>
2025-04-03 00:40:49 +0800

    add align container

---
+++ containers/Align.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { useChildren } from "../utils/elements";
+
+interface AlignProps {
+  children: React.ReactNode | React.ReactNode[];
+  width?: string;
+  height?: string;
+  vertical?: 'top' | 'center' | 'bottom' | boolean;
+  horizontal?: 'left' | 'center' | 'right' | boolean;
+  style?: React.CSSProperties;
+  [x:string]: any;
+}
+
+const useRest = (props: AlignProps) => {
+  const { children, width, height, vertical, horizontal, style, ...rest } = props;
+  return rest;
+}
+
+const useVerticalStyle = (vertical?: 'top' | 'center' | 'bottom' | boolean) => {
+  const value = vertical === true ? 'center' : vertical;
+  const ret: React.CSSProperties = {};
+  switch (value) {
+    case 'top':
+      ret.marginTop = '0';
+      ret.marginBottom = 'auto';
+      ret.height = 'fit-content';
+      break;
+
+    case 'center':
+      ret.marginTop = 'auto';
+      ret.marginBottom = 'auto';
+      ret.height = 'fit-content';
+      break;
+    
+    case 'bottom':
+      ret.marginTop = 'auto';
+      ret.marginBottom = '0';
+      ret.height = 'fit-content';
+      break;
+
+    default:
+      ret.marginTop = '0';
+      ret.marginBottom = '0';
+      ret.height = '100%';
+  }
+  return ret;
+}
+
+const useHorizontalStyle = (horizontal?: 'left' | 'center' | 'right' | boolean) => {
+  const value = horizontal === true ? 'center' : horizontal;
+  const ret: React.CSSProperties = {};
+  switch (value) {
+    case 'left':
+      ret.marginLeft = '0';
+      ret.marginRight = 'auto';
+      ret.width = 'fit-content';
+      break;
+
+    case 'center':
+      ret.marginLeft = 'auto';
+      ret.marginRight = 'auto';
+      ret.width = 'fit-content';
+      break;
+    
+    case 'right':
+      ret.marginLeft = 'auto';
+      ret.marginRight = '0';
+      ret.width = 'fit-content';
+      break;
+
+    default:
+      ret.marginLeft = '0';
+      ret.marginRight = '0';
+      ret.width = '100%';
+  }
+  return ret;
+}
+
+const Align: React.FC<AlignProps> = (props) => {
+  const children = useChildren(props.children);
+  const rest = useRest(props);
+
+  const parentStyle: React.CSSProperties = {
+    display: 'flex',
+    width: props.width || '100%',
+    height: props.height || '100%',
+    ...props.style
+  };
+
+  const childStyle: React.CSSProperties = {
+    ...useVerticalStyle(props.vertical),
+    ...useHorizontalStyle(props.horizontal)
+  };
+
+  return (
+    <div style={parentStyle} {...rest}>
+      <div style={childStyle}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Align;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import Scrollable from './containers/Scrollable';
+import Align from './containers/Align';
 import JoinElements from './elements/JoinElements';
 
 export {
   Scrollable,
+  Align,
 
   JoinElements,
 };
